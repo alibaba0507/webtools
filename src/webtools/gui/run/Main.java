0730987d93fd25a7e3fbaa70090f957a5d1bddad
alibaba0507 <fx2go4u@gmail.com>
2019-06-29 19:26:33 +0200

    First Commit

--- src/webtools/gui/run/Main.java
+++ src/webtools/gui/run/Main.java
@@ -14,27 +14,36 @@ import webtools.net.SSLCertificates;
  * @author alibaba0507
  */
 public class Main {
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
         java.lang.System.setProperty("https.protocols", "TLSv1");
         SSLCertificates.ignoreCertificates();
         System.out.println(System.getProperty("https.protocols"));
         final SplashScreen splash = new SplashScreen("/images/article-spinning.jpg");
         splash.setVisible(true);
-        
-        Timer t = new Timer();
-       
-        t.schedule(new TimerTask() {
+        long start = System.currentTimeMillis();
+        new Thread(new Runnable() {
             @Override
             public void run() {
-                //  while (true) {                    
-                Thread.currentThread().yield();
-                splash.close();
-                //frame.setVisible(true);
-                //InnerFramesUI.getInstance().moveHelpFiles();
-                // }
-            }
-        }, 5000);
+                try {
+                    while (WebToolMainFrame.instance == null) {
+                       Thread.sleep(500);
+                    }
+                    while (System.currentTimeMillis() - start < 5000)
+                    {
+                        Thread.sleep(500);
+                    }
+                    splash.close();
+                    WebToolMainFrame.instance.setVisible(true);
+                } catch (InterruptedException ex) {
 
+                }
+
+            }
+        }).start();
+        
+        new WebToolMainFrame();
+        
+        
     }
 }
--- src/webtools/gui/run/WebToolMainFrame.java
+++ src/webtools/gui/run/WebToolMainFrame.java
@@ -5,14 +5,58 @@
  */
 package webtools.gui.run;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import javax.swing.JDesktopPane;
 import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JPopupMenu;
+import javax.swing.JToolBar;
+import org.json.JSONArray;
+import org.json.simple.parser.JSONParser;
+
+ 
 
 /**
  *
  * @author alibaba0507
  */
 public class WebToolMainFrame extends JFrame{
+    public static WebToolMainFrame instance;
+    private final JDesktopPane desktop;
+    private final JToolBar toolBar;
+    private final JMenuBar menuBar;
+    private JPopupMenu popup;
+    private JPopupMenu popupProj;
     public WebToolMainFrame(){
         super("WebTools");
+        // do some extra work and then
+        desktop = new JDesktopPane();
+        toolBar = new JToolBar();
+        menuBar = new JMenuBar();
+        createMenuBar();
+        setJMenuBar(menuBar);
+        
+        if (WebToolMainFrame.instance == null) {
+            WebToolMainFrame.instance = this;
+        }
+    }
+    private void createMenuBar() {
+        JSONParser jsonParser = new JSONParser();
+         
+        try (FileReader reader = new FileReader("employees.json"))
+        {
+            //Read JSON file
+            Object obj = jsonParser.parse(reader);
+            JSONArray jsonMenu = (JSONArray) obj;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (org.json.simple.parser.ParseException e) {
+            e.printStackTrace();
+        }
     }
 }
